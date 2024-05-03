23abcb7767a9eab9f0adf4b26b7eb567c86e2fac
yogskr <yogakrisanta@gmail.com>
2024-05-03 17:16:06 +0700

    Added class names to <nav> and <div> tags

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import Links from './links/Links';
+import styles from './navbar.module.css';
 
 function Navbar() {
   return (
-    <nav>
-      <div>Logo</div>
+    <nav className={styles.container}>
+      <div className={styles.logo}>upnflow</div>
       <div>
         <Links />
       </div>
